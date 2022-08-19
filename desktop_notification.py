0b0bbe45c92522f31eadeeafa8dc5a0b5a2986a2
Levon Martirosyan <levon.martirosyan@dataart.com>
2022-08-19 11:27:06 +0400

    refactoring cod

--- desktop_notification.py
+++ desktop_notification.py
@@ -1,12 +1,12 @@
-import  time
+import time
 
 from plyer import notification
 
-if __name__ =='__main__':
+if __name__ == '__main__':
     while True:
         notification.notify(
-            title = 'ALERT!!!',
-            message = 'Take a break!',
-            timeout = 10
+            title='ALERT!!!',
+            message='Take a break!',
+            timeout=10
         )
         time.sleep(5)
